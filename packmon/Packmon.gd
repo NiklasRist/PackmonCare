b66b772c5c37fdb59804f6c69279d4520135bba9
NiklasRistCds <79253498+NiklasRistCds@users.noreply.github.com>
2022-06-05 13:57:49 +0200

    Add files via upload

--- packmon/Packmon.gd
+++ packmon/Packmon.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
-
 class_name Packmon
 
+export var Packmonname : String
+
 export var onLevelupHp : int
+export var onLevelupSpd : int
 export var onLevelupAtk : int
 export var onLevelupDef : int
-export var onLevelupSpd : int
+
 
 export var attackenInBesitz : PoolIntArray
 
@@ -17,9 +19,10 @@ export var epRequired : int
 export var ep : int
 export var lvl : int
 export var hp : int
+export var spd : int
 export var atk : int
 export var def : int
-export var spd : int
+
 
 #SW
 export var paralyse : bool
@@ -29,6 +32,8 @@ export var brennend : bool
 export var regenerierend : bool
 export var eingefroren : bool
 export var verwirrt : bool
+
+export (Texture) var texture
 
 func _init():
 	type = ["normal"]
