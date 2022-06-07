2c25dadb2dca4390b75d631b0498e44f4a967a46
NiklasRistCds <79253498+NiklasRistCds@users.noreply.github.com>
2022-06-07 18:10:21 +0200

    Add files via upload

---
+++ packmon/Dorie/Dorie.gd
@@ -0,0 +1,28 @@
+extends "res://packmon/Packmon.gd"
+
+func _init():
+	Packmonname="Dorie"
+	
+	lvl=1
+	ep=0
+	epRequired=12
+	type = ["water"]
+	
+	hp=42
+	spd=18
+	atk=19
+	def=21
+	
+	onLevelupHp=41
+	onLevelupSpd=19
+	onLevelupAtk=18
+	onLevelupDef=22
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ packmon/attacken/Attacken.gd
@@ -0,0 +1,82 @@
+extends Node2D
+#import Packmon.gd
+const Packmon = preload("res://packmon/Packmon.gd") # Relative path
+onready var PackmonVar = Packmon.new()
+
+#still needs to be imported from Statuseffekte
+export var paralyse = false
+export var vergiftung = false
+export var schlafend = false
+export var brennend = false 
+export var regenerierend = false
+export var eingefroren = false 
+export var verwirrt = false
+export var angst = false
+#0=enemy
+#1=missed
+#2=user
+var effecttarget
+var effectprob
+
+var dmg
+#fist number is change of user sp, second of enemy sp
+var atkChange=[0, 0]
+var defChange=[0, 0]
+var spdChange=[0, 0]
+var hpChange=[0, 0]
+
+var atkChangeUser=false
+var atkChangeEnemy=false
+var defChangeUser=false
+var defChangeEnemy=false
+var spdChangeUser=false
+var spdChangeEnemy=false
+var hpChangeUser=false
+var hpChangeEnemy=false
+var changetarget=[atkChangeUser, atkChangeEnemy, defChangeUser, defChangeEnemy, spdChangeUser, spdChangeEnemy, hpChangeUser, hpChangeEnemy]
+
+var atkDefault
+var defDefault
+var spdDefault
+var hpDefault
+
+var attackname
+#create slider for damage percentual 0-3.5
+export (float, 3.5) var dmgp 
+#create slider for probability 0-1
+export (float, 1) var prob 
+#create slider for attack loading time 0-5
+export (int, 5) var atkLoad
+#create slider for duration 1-1000000000000
+export (int, 1, 1000000000000) var dur
+#create slider for repition 0-20
+export (int, 20) var rep
+#create checkbox for effect
+export (bool) var efct = [paralyse, vergiftung, schlafend, brennend, regenerierend, eingefroren, verwirrt]
+#create slider for effect probability 0-1
+export (float, 1) var efctprob
+#create slider for SP changes percentual
+export (float, -0.95, 2) var atkp
+export (float, -0.95, 2) var defp
+export (float, -0.95, 2) var hpp
+export (float, -0.95, 2) var spdp
+#create slider for priority
+export (int, 1, 10) var prio
+#create slider for target 
+#0=enemy
+#1=missed
+#2=user
+export (int, 2) var target
+
+func _init():
+	dmgp=0
+	prob=0
+	dmg = dmgp #* PackmonVar.atk
+	
+	atkChange=atkp#*atkDefault
+	defChange=defp#*defDefault
+	spdChange=spdp#*spdDefault
+	hpChange=hpp#*hpDefault
+	
+	
+	
