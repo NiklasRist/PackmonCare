929f6468bdf7046ef3c8396612300cdd396e284f
NiklasRistCds <79253498+NiklasRistCds@users.noreply.github.com>
2022-06-07 17:32:44 +0200

    Add files via upload

---
+++ packmon/attacken/basic/silent battle cry.gd
@@ -0,0 +1,13 @@
+extends "res://packmon/attacken/Attacken.gd"
+
+func _init():
+	attackname="Silent Battle Cry"
+	dmgp=0
+	prob=1
+	atkLoad=0
+	dur=1
+	rep=0
+	atkChange=[0, -0.1]
+	prio=10
+	target=0
+	changetarget=[false, true, false, false, false, false, false, false]
