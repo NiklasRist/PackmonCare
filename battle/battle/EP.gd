b1947a1980c148e6f9134369870b881bc846bb9f
NiklasRistCds <79253498+NiklasRistCds@users.noreply.github.com>
2022-06-08 19:10:18 +0200

    Add files via upload

---
+++ battle/battle/EP.gd
@@ -0,0 +1,14 @@
+extends ProgressBar
+
+const Rabbiflaflam = preload("res://packmon/Rabbiflaflam/Rabbiflaflam.gd") # Relative path
+onready var RabbiflaflamVar = Rabbiflaflam.new()
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	value=RabbiflaflamVar.ep
+	max_value=RabbiflaflamVar.epRequired
