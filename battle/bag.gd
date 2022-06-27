7997b4a04cc6b0857b2f8673d91782806082a139
NiklasRistCds <79253498+NiklasRistCds@users.noreply.github.com>
2022-06-27 08:29:38 +0200

    Add files via upload

--- battle/bag.gd
+++ battle/bag.gd
@@ -6,8 +6,6 @@ var player = player_.new()
 var num =1
 
 func _pressed():
-	#get_tree().change_scene("res://inventory/Inventory.tscn")
-	#get_parent().get_parent().get_parent().get_child(5).visible = true
 	player.packmon_space[0][num]=get_parent().get_parent().get_parent().Packname
 	player.packmon_space[1][num]=get_parent().get_parent().get_parent().Packlvl
 	player.packmon_space[2][num]=get_parent().get_parent().get_parent().Packattacks
